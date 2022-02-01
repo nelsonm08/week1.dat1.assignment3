ae91954053418798a27edd64f0f5faeb875746e1
nelsonm08 <98229012+nelsonm08@users.noreply.github.com>
2022-02-01 22:19:54 +0530

    Add files via upload

---
+++ assign3/Palindrome.java
@@ -0,0 +1,21 @@
+package week1.day2.assign3;
+
+public class Palindrome {
+	
+	@SuppressWarnings("null")
+public static void main(String[] args) {
+	String value = "madam", rev = "";
+	int length = value.length();
+	for(int i = length - 1;i >= 0;i--) {
+	rev = rev + value.charAt(i);
+			}
+	if(value.equals(rev)) {
+		System.out.println("it is palindrome");
+	}
+	else {
+	    System.out.println("it is not palindrome");
+	}
+	
+			
+}
+}
